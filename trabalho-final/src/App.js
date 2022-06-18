65fb17ae35ae7c32b849cbce98ddf8af68736bf5
Aysha Alessandra <aysha.alessandra.oliver@gmail.com>
2022-06-18 12:10:03 -0300

    com responsividade

---
+++ trabalho-final/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Inicio } from './pages/paginaHome/home';
+function App () {
+  return (
+    <>
+    <Inicio/>
+    </>
+  );
+};
+
+export default App;
